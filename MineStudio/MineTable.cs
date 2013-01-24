4260a04d5519b39ac78276d1ef282bd4b2e0a4c8
karata@waptop <vabc3h@gmail.com>
2013-01-25 00:01:41 +0800

    Init

---
+++ MineStudio/MineTable.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MineStudio
+{
+    class MineTable
+    {
+        public MineTable(int H)
+        {
+
+        }
+
+
+        public int Height { get; set; }
+         //Width, MineCount;
+
+        public override string ToString()
+        {
+            return "Mtab-le";
+        }
+    }
+}
---
+++ MineStudio/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MineStudio
+{
+   
+    class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            MineTable mt=new MineTable(4);
+            System.Console.WriteLine(mt);
+
+            System.Console.WriteLine("Miner!");
+
+          
+        }
+    }
+}
