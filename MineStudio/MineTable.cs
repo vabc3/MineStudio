6770c4558a5642229ccafd95f200abd03e4cd947
karata@waptop <vabc3h@gmail.com>
2013-01-26 22:35:19 +0800

    St

--- MineStudio/MineTable.cs
+++ MineStudio/MineTable.cs
@@ -7,13 +7,19 @@ namespace MineStudio
 {
     enum MineStatus
     {
-        UNKNOW,NOMINE,MINE
+        ILLEGAL, UNKNOW, NOMINE, ISMINE
     }
 
     class MineTable
     {
         private static readonly int UNKNOW = -2;
         private static readonly int ISMINE = -1;
+        private int[,]  Table;
+        private int Current;
+        public int Height { get; set; }
+        public int Width { get; set; }
+        public int MineCount { get; set; }
+
 
         public MineTable(int Height, int Width, int MineCount)
         {
@@ -21,16 +27,109 @@ namespace MineStudio
             this.Width      = Width;
             this.MineCount  = MineCount;
             this.Table      = new int[Height, Width];
+            this.Current    = 0;
             for (int i = 0; i < Height; i++)
                 for (int j = 0; j<Width; j++)
-                    this.Table[i,j]=UNKNOW;
+                    this.Table[i, j]=UNKNOW;
         }
 
-        public int Height       { get; set; }
-        public int Width        { get; set; }
-        public int MineCount    { get; set; }
-        private int[,]  Table;
+        public MineStatus SetStatus(int x, int y, MineStatus Status, int Num=-1)
+        {
+            if ((x<0 || x>=Width || y<0 || y>=Height || UNKNOW!=Table[y, x]))
+                return MineStatus.ILLEGAL;
+            MineStatus rt=Status;
+            switch (Status)
+            {
+                case MineStatus.NOMINE:
+                    if (Num>=0 && Num<=8)
+                        Table[y, x] = Num;
+                    else
+                        rt          = MineStatus.ILLEGAL;
+                    break;
+                case MineStatus.ISMINE:
+                    if (Current<MineCount)
+                    {
+                        Table[y, x]     = ISMINE;
+                        Current++;
+                    }
+                    break;
+                default:
+                    break;
+            }
+            return rt;
+        }
 
+        public MineStatus GetStatus(int x, int y, out int Num)
+        {
+            Num = -1;
+
+            if (x<0 || x>=Width || y<0 || y>=Height)
+                return MineStatus.ILLEGAL;
+
+            Num=Table[y, x];
+            if (UNKNOW==Num)
+                return MineStatus.UNKNOW;
+            else if (ISMINE==Num)
+                return MineStatus.ISMINE;
+
+            return MineStatus.NOMINE;
+        }
+
+        private readonly int[,] Direct= { {1,0},{1,1},{0,1},{-1,1},
+                                          {-1,0},{-1,-1},{0,-1},{1,-1}
+                                        };
+
+        private int IsMine(int x, int y)
+        {
+            if (x<0 || x>=Width || y<0 || y>=Height)
+                return 0;
+            else
+                return (ISMINE==Table[x, y]?1:0);
+        }
+
+        private int Sum(int x, int y)
+        {
+            if (x<0 || x>=Width || y<0 || y>=Height)
+                return -1;
+            int count=0;
+
+            for (int i=0; i<8; i++)
+            {
+                count+=IsMine(y+Direct[i, 0], x+Direct[i, 1]);
+            }
+
+            return count;
+        }
+
+        public void Conduce()
+        {
+            if (Current==MineCount)
+            {
+                Console.WriteLine("Can !");
+                for (int j=0; j<Height; j++)
+                    for (int i=0; i<Width; i++)
+                        if (UNKNOW==Table[j, i])
+                            Table[j, i]=Sum(i, j);
+            }
+        }
+
+        public void Show()
+        {
+            for (int y=0; y<Height; y++)
+            {
+                for (int x=0; x<Width; x++)
+                {
+                    int st=Table[y, x];
+                    if (st>=0)
+                        Console.Write(st);
+                    else if (st==UNKNOW)
+                        Console.Write('U');
+                    else
+                        Console.Write('M');
+                }
+                Console.WriteLine();
+            }
+        }
 
         public override string ToString()
         {
--- MineStudio/Program.cs
+++ MineStudio/Program.cs
@@ -10,9 +10,21 @@ namespace MineStudio
         static void Main(string[] args)
         {
             MineTable mt=new MineTable(9,9,10);
-            System.Console.WriteLine(mt);
-            System.Console.WriteLine("Miner!");
-            //System.Console.WriteLine(mt.
+            Console.WriteLine(mt);
+            Console.WriteLine("Miner!");
+            mt.SetStatus(2, 0, MineStatus.ISMINE);
+            mt.SetStatus(4, 0, MineStatus.ISMINE);
+            mt.SetStatus(5, 0, MineStatus.ISMINE);
+            mt.SetStatus(7, 0, MineStatus.ISMINE);
+            mt.SetStatus(3, 1, MineStatus.ISMINE);
+            mt.SetStatus(5, 2, MineStatus.ISMINE);
+            mt.SetStatus(5, 3, MineStatus.ISMINE);
+            mt.SetStatus(8, 4, MineStatus.ISMINE);
+            mt.SetStatus(0, 6, MineStatus.ISMINE);
+            mt.SetStatus(4, 8, MineStatus.ISMINE);
+            mt.Show();
+            mt.Conduce();
+            mt.Show();
         }
     }
 }
