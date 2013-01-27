b5a0d5d72d2938dcfd5e9b66d4edd5eb085223a9
karata@waptop <vabc3h@gmail.com>
2013-01-28 00:22:46 +0800

    Table Source Update

--- MineStudioBase/MineTable.cs
+++ MineStudioBase/MineTable.cs
@@ -1,111 +1,179 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.ComponentModel;
 
 namespace MineStudio
 {
-    public enum MineStatus
+    public enum CellStatus
     {
-        ILLEGAL, UNKNOW, NOMINE, ISMINE
+        Unknow, Mine, Ground
+    }
+
+    public class MineCell : INotifyPropertyChanged
+    {
+        public MineCell(int x, int y)
+        {
+            X           = x;
+            Y           = y;
+            MineCount   = -1;
+            UnknowCount = -1;
+            Covered     = true;
+            Status      = CellStatus.Unknow;
+        }
+
+        public readonly int X;
+        public readonly int Y;
+        private int _mineCount;
+
+        /// <summary>
+        /// Number Of Mines Near
+        /// </summary>
+        public int MineCount
+        {
+            get { return _mineCount; }
+            set { _mineCount = value;
+            OnPropertyChanged();
+            }
+        }
+
+        public int UnknowCount { get; set; }
+        public bool Covered { get; private set; }
+        public CellStatus Status { get; private set; }
+        public IEnumerable<MineCell> NearCells { get; set; }
+
+        public bool SetStatusMine()
+        {
+            Status  = CellStatus.Mine;
+            Covered = false;
+            return true;
+        }
+
+        public bool SetStatusGround(int num=-1)
+        {
+            if (num >= -1 && num < 9)
+            {
+                Status = CellStatus.Ground;
+                if (num != -1)
+                {
+                    MineCount       = num;
+                    Covered = false;
+                }
+                return true;
+            }
+            return false;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName="")
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+            Console.WriteLine("Debug!!!");
+        }
     }
 
     public class MineTable
     {
+        public readonly int Height;
+        public readonly int Width;
+        public readonly int MineCount;
+        public int Current { get; private set; }
+        public MineCell[] Table { get; private set; }
 
-        private static readonly int UNKNOW = -2;
-        private static readonly int ISMINE = -1;
-        public int[,]  Table;
-        private int Current;
-        public int Height { get; set; }
-        public int Width { get; set; }
-        public int MineCount { get; set; }
-
-
-        public MineTable(int Height, int Width, int MineCount)
+        public MineTable(int height, int width, int mineCount)
         {
-            this.Height     = Height;
-            this.Width      = Width;
-            this.MineCount  = MineCount;
-            this.Table      = new int[Height, Width];
-            this.Current    = 0;
-            for (int i = 0; i < Height; i++)
-                for (int j = 0; j<Width; j++)
-                    this.Table[i, j]=UNKNOW;
+            Height      = height;
+            Width       = width;
+            MineCount   = mineCount;
+            Current     = 0;
+            Table       = new MineCell[Height*Width];
+            for (var j=0; j<Height; j++)
+                for (var i = 0; i < Width; i++)
+                    Table[GetIndex(i, j)] = new MineCell(i, j);
+            foreach (var it in Table)
+                it.NearCells=(from a in dire where IndexValid(it.X+a[0], it.Y+a[1]) select Table[GetIndex(it.X+a[0], it.Y+a[1])]).ToList();
         }
 
-        public MineStatus SetStatus(int x, int y, MineStatus status, int Num=-1)
+        private bool IndexValid(int x, int y)
         {
-            if ((x<0 || x>=Width || y<0 || y>=Height || UNKNOW!=Table[y, x]))
-                return MineStatus.ILLEGAL;
-            MineStatus rt=status;
+            return (x >= 0 && x < Width && y >= 0 && y < Height);
+        }
+
+        private int GetIndex(int x, int y)
+        {
+            return y * Width+x;
+        }
+
+        public bool SetStatus(int x, int y, CellStatus status, int num=-1)
+        {
+            if (!IndexValid(x, y))
+                return false;
+            int index = GetIndex(x, y);
             switch (status)
             {
-                case MineStatus.NOMINE:
-                    if (Num>=0 && Num<=8)
-                        Table[y, x] = Num;
-                    else
-                        rt          = MineStatus.ILLEGAL;
+                case CellStatus.Unknow:
                     break;
-                case MineStatus.ISMINE:
-                    if (Current<MineCount)
-                    {
-                        Table[y, x]     = ISMINE;
-                        Current++;
-                    }
+                case CellStatus.Mine:
+                    return Table[index].SetStatusMine();
                     break;
+                case CellStatus.Ground:
+                    return Table[index].SetStatusGround(num);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException("status");
             }
-            return rt;
+            return true;
         }
 
-        public MineStatus GetStatus(int x, int y, out int Num)
+
+        public void UpdateCount()
         {
-            Num = -1;
-
-            if (x<0 || x>=Width || y<0 || y>=Height)
-                return MineStatus.ILLEGAL;
-
-            Num=Table[y, x];
-            if (UNKNOW==Num)
-                return MineStatus.UNKNOW;
-            else if (ISMINE==Num)
-                return MineStatus.ISMINE;
-
-            return MineStatus.NOMINE;
-        }
-
-        private readonly int[,] Direct= { {1,0},{1,1},{0,1},{-1,1},
-                                          {-1,0},{-1,-1},{0,-1},{1,-1}
-                                        };
-
-        private int IsMine(int x, int y)
-        {
-            if (x<0 || x>=Width || y<0 || y>=Height)
-                return 0;
-            else
-                return (ISMINE==Table[x, y]?1:0);
-        }
-
-        private int Sum(int x, int y)
-        {
-            if (x<0 || x>=Width || y<0 || y>=Height)
-                return -1;
-            int count=0;
-
-            for (int i=0; i<8; i++)
+            Current = 0;
+            foreach (var mineCell in Table)
             {
-                count+=IsMine(y+Direct[i, 0], x+Direct[i, 1]);
+                if (CellStatus.Mine == mineCell.Status)
+                    Current++;
+                int mc = 0;
+                int uc = 0;
+                foreach (var it in mineCell.NearCells)
+                {
+                    switch (it.Status)
+                    {
+                        case CellStatus.Unknow:
+                            uc++;
+                            break;
+                        case CellStatus.Mine:
+                            mc++;
+                            break;
+                        case CellStatus.Ground:
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
+                }
+                mineCell.MineCount=mc;
+                mineCell.UnknowCount=uc;
             }
-
-            return count;
         }
+
+        private readonly List<int[]> dire=new List<int[]> 
+        {
+           new[]{1,0},new[]{1,1},new[]{0,1},new[]{-1,1},
+           new[]{-1,0},new[]{-1,-1},new[]{0,-1},new[]{1,-1}
+        };
+
 
         public void Conduce()
         {
+            UpdateCount();
             if (Current==MineCount)
             {
                 Console.WriteLine("Can !");
-                for (int j=0; j<Height; j++)
-                    for (int i=0; i<Width; i++)
-                        if (UNKNOW==Table[j, i])
-                            Table[j, i]=Sum(i, j);
+                for (var i=0; i<Width*Height; i++)
+                    if (CellStatus.Unknow==Table[i].Status)
+                        Table[i].SetStatusGround(Table[i].MineCount);
             }
         }
 
@@ -115,13 +183,25 @@ namespace MineStudio
             {
                 for (int x=0; x<Width; x++)
                 {
-                    int st=Table[y, x];
-                    if (st>=0)
-                        Console.Write(st);
-                    else if (st==UNKNOW)
-                        Console.Write('U');
-                    else
-                        Console.Write('M');
+                    int idx=GetIndex(x, y);
+                    var st=Table[idx].Status;
+                    switch (st)
+                    {
+                        case CellStatus.Unknow:
+                            Console.Write('U');
+                            break;
+                        case CellStatus.Mine:
+                            Console.Write('M');
+                            break;
+                        case CellStatus.Ground:
+                            if (Table[idx].Covered)
+                                Console.Write("C");
+                            else
+                                Console.Write(Table[idx].MineCount);
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException();
+                    }
                 }
                 Console.WriteLine();
             }
--- MineStudioGUI/MainWindow.xaml.cs
+++ MineStudioGUI/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Data;
 using System.Windows;
 using System.ComponentModel;
+using System.Collections.Generic;
 
 namespace MineStudio.GUI
 {
@@ -10,6 +11,7 @@ namespace MineStudio.GUI
     /// </summary>
     public partial class MainWindow : Window
     {
+        private MineTable _mineTable;
         public MainWindow()
         {
             InitializeComponent();
@@ -24,65 +26,34 @@ namespace MineStudio.GUI
 
         private void ButtonRandom_Click(object sender, RoutedEventArgs e)
         {
-            MineTable mt=new MineTable(9, 9, 10);
+            _mineTable  = new MineTable(9, 9, 10);
             Console.WriteLine(Properties.Resources.MainWindow_ButtonRandom_Click_Miner_);
-            mt.SetStatus(2, 0, MineStatus.ISMINE);
-            mt.SetStatus(4, 0, MineStatus.ISMINE);
-            mt.SetStatus(5, 0, MineStatus.ISMINE);
-            mt.SetStatus(7, 0, MineStatus.ISMINE);
-            mt.SetStatus(3, 1, MineStatus.ISMINE);
-            mt.SetStatus(5, 2, MineStatus.ISMINE);
-            mt.SetStatus(5, 3, MineStatus.ISMINE);
-            mt.SetStatus(8, 4, MineStatus.ISMINE);
-            mt.SetStatus(0, 6, MineStatus.ISMINE);
-            mt.SetStatus(4, 8, MineStatus.ISMINE);
-            mt.Show();
-            mt.Conduce();
-
-            int[,] TABLE = mt.Table;
-            DataTable dt = new DataTable();
-            for (int i = 0; i < TABLE.GetLength(1); i++)
-                dt.Columns.Add(i.ToString(), typeof(int));
-            for (int i = 0; i < TABLE.GetLength(0); i++)
-            {
-                DataRow dr = dt.NewRow();
-                for (int j = 0; j < TABLE.GetLength(1); j++)
-                    dr[j] = TABLE[i, j];
-                dt.Rows.Add(dr);
-            }
-
-            
-            DataGrid1.ColumnWidth = 40;
-            DataGrid1.RowHeight   = 40;
-            //DataGrid1.ItemsSource = dt.AsDataView();
-
-            customerList = new BindingList<DemoCustomer>();
-            customerList.Add(new DemoCustomer());
-            customerList.Add(new DemoCustomer());
-            customerList.Add(new DemoCustomer());
-            DataGrid1.ItemsSource = customerList;
-
+            _mineTable.SetStatus(2, 0, CellStatus.Mine);
+            _mineTable.SetStatus(4, 0, CellStatus.Mine);
+            _mineTable.SetStatus(5, 0, CellStatus.Mine);
+            _mineTable.SetStatus(7, 0, CellStatus.Mine);
+            _mineTable.SetStatus(3, 1, CellStatus.Mine);
+            _mineTable.SetStatus(5, 2, CellStatus.Mine);
+            _mineTable.SetStatus(5, 3, CellStatus.Mine);
+            _mineTable.SetStatus(8, 4, CellStatus.Mine);
+            _mineTable.SetStatus(0, 6, CellStatus.Mine);
+            _mineTable.SetStatus(4, 8, CellStatus.Mine);
+            DataGrid1.ItemsSource = _mineTable.Table;
         }
-
-        private BindingList<DemoCustomer> customerList;
 
         private void ButtonUpdate_Click(object sender, RoutedEventArgs e)
         {
             int a = 2;
         }
-    }
 
-    public class DemoCustomer
-    {
-        public Guid Guid { get; set; }
-        public string CustomerName { get; set; }
-        public string PhoneNumber { get; set; }
-        public DemoCustomer()
+        private void ButtonDeduce_Click(object sender, RoutedEventArgs e)
         {
-            Guid = Guid.NewGuid();
-            CustomerName = "中";
-            PhoneNumber = "(312)555-0100";
+            if (_mineTable!=null)
+                _mineTable.Conduce();
+          //  DataGrid1.ItemsSource = _mineTable.Table;
         }
+
+
+
     }
 }
-
