1c2d16583508b57b3c22019473f49f67626c1aba
karata@waptop <vabc3h@gmail.com>
2013-01-28 20:43:14 +0800

    D2

--- MineStudioTest/W7MineIdentifierTest.cs
+++ MineStudioTest/W7MineIdentifierTest.cs
@@ -51,20 +51,21 @@ namespace MineStudio.Test
         /// <summary>
         ///GetCellInfo 的测试
         ///</summary>
-        //[TestMethod()]
-        //public void GetCellInfoTest()
-        //{
-        //    W7MineIdentifier target = new W7MineIdentifier(); 
-        //    Bitmap data = null; 
-        //    CellStatus status; 
-        //    CellStatus statusExpected = new CellStatus(); 
-        //    int n = 0; // TODO: 初始化为适当的值
-        //    bool expected = false; // TODO: 初始化为适当的值
-        //    bool actual;
-        //    actual = target.GetCellInfo(data, out status, n);
-        //    Assert.AreEqual(statusExpected, status);
-        //    Assert.AreEqual(expected, actual);
-        //}
+//[TestMethod()]
+//public void GetCellInfoTest()
+//{
+//    W7MineIdentifier target = new W7MineIdentifier(); 
+//    Bitmap data = null; 
+//    CellStatus status; 
+//    CellStatus statusExpected = new CellStatus(); 
+//    int n = 0; // TODO: 初始化为适当的值
+//    bool expected = false; // TODO: 初始化为适当的值
+//    bool actual;
+//    actual = target.GetCellInfo(data, out status, n);
+//    Assert.AreEqual(statusExpected, status);
+//    Assert.AreEqual(expected, actual);
+//}
+        private const String prefix = @"..\..\..\MineStudioTest\ImageCases\";
 
         /// <summary>
         ///GetTableInfo 的测试
@@ -72,16 +73,60 @@ namespace MineStudio.Test
         [TestMethod()]
         public void GetTableInfoTest1()
         {
-            String prefix=@"..\..\..\MineStudioTest\ImageCases\";
             W7MineIdentifier target = new W7MineIdentifier();
-            string str = System.Environment.CurrentDirectory;
-            //Bitmap data = new Bitmap("/MineStudioTest;component/ImageCases/Table1.bmp");
             Bitmap data = new Bitmap(prefix+"Table1.bmp"); 
-            int width;
-            int height;
+            int width,height;
             bool actual = target.GetTableInfo(data, out width, out height);
             Assert.AreEqual(9, width);
             Assert.AreEqual(9, height);
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void GetTableInfoTest2()
+        {
+            W7MineIdentifier target = new W7MineIdentifier();
+            Bitmap data = new Bitmap(prefix+"Table2.png");
+            int width,height;
+            bool actual = target.GetTableInfo(data, out width, out height);
+            Assert.AreEqual(16, width);
+            Assert.AreEqual(16, height);
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void GetTableInfoTest3()
+        {
+            W7MineIdentifier target = new W7MineIdentifier();
+            Bitmap data = new Bitmap(prefix+"Table3.png");
+            int width,height;
+            bool actual = target.GetTableInfo(data, out width, out height);
+            Assert.AreEqual(30, width);
+            Assert.AreEqual(16, height);
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void GetTableInfoTest4()
+        {
+            W7MineIdentifier target = new W7MineIdentifier();
+            Bitmap data = new Bitmap(prefix+"Table4.png");
+            int width,height;
+            bool actual = target.GetTableInfo(data, out width, out height);
+            Assert.AreEqual(30, width);
+            Assert.AreEqual(24, height);
+            Assert.AreEqual(true, actual);
+        }
+
+        [TestMethod()]
+        public void GetTableInfoTest5()
+        {
+            W7MineIdentifier target = new W7MineIdentifier();
+            Bitmap data = new Bitmap(prefix+"Table5.png");
+            int width,height;
+            bool actual = target.GetTableInfo(data, out width, out height);
+            Assert.AreEqual(18, width);
+            Assert.AreEqual(17, height);
             Assert.AreEqual(true, actual);
         }
     }
